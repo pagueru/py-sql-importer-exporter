a2ce1282b19705a6c720a601c1ea8bd22412dd2a
Raphael Coelho <raphael.phael@gmail.com>
2025-06-25 21:09:55 -0300

    init: atualizacao de backup da main

--- data/sql/Botmaker-14.sql
+++ data/sql/Botmaker-14.sql
@@ -1,25 +1,19 @@
---1-20250620-adhoc-wpp-arraia_de_ofertas_NEWBACON
+--1-20250625_adhoc_wpp_relacionamento_incentivo_cadastro_NEWBACON
 SELECT CAST(CONCAT('55',CELULAR) AS NVARCHAR(20)) AS phone
       ,CAST(PRIMEIRO_NOME AS NVARCHAR(20)) AS chicoFirstName
-      ,CAST([HASH] AS NVARCHAR(5)) AS chicoLinkHash
-      ,CAST('https://unilevertrade.s3.dualstack.sa-east-1.amazonaws.com/WhatsApp_Botmaker/000_Trade_UFS_Promos_de_junho_00-00_wpp.png' AS VARCHAR(1000)) AS imageURL
-  FROM Unilever_000_DBM.dbo.[20250620-adhoc-wpp-arraia_de_ofertas]
+  FROM Unilever_000_DBM.dbo.[20250625_adhoc_wpp_relacionamento_incentivo_cadastro]
  WHERE ORIGEM LIKE '%CONTROLE%'
-   AND CELULAR IN ('11991371931','18996698725')
+   AND CELULAR IN ('11991371931','18996698725','11998055353')
 
---1-20250620-adhoc-wpp-arraia_de_ofertas_CLIENTE
+--1-20250625_adhoc_wpp_relacionamento_incentivo_cadastro_CLIENTE
 SELECT CAST(CONCAT('55',CELULAR) AS NVARCHAR(20)) AS phone
       ,CAST(PRIMEIRO_NOME AS NVARCHAR(20)) AS chicoFirstName
-      ,CAST([HASH] AS NVARCHAR(5)) AS chicoLinkHash
-      ,CAST('https://unilevertrade.s3.dualstack.sa-east-1.amazonaws.com/WhatsApp_Botmaker/000_Trade_UFS_Promos_de_junho_00-00_wpp.png' AS VARCHAR(1000)) AS imageURL
-  FROM Unilever_000_DBM.dbo.[20250620-adhoc-wpp-arraia_de_ofertas]
+  FROM Unilever_000_DBM.dbo.[20250625_adhoc_wpp_relacionamento_incentivo_cadastro]
  WHERE ORIGEM LIKE '%CONTROLE%'
-   AND CELULAR IN ('11991371931','18996698725'
+   AND CELULAR IN ('11991371931','18996698725','11998055353'
                   ,'38998310055','11945790149','11964920611')
 
---1-20250620-adhoc-wpp-arraia_de_ofertas_FINAL
+--1-20250625_adhoc_wpp_relacionamento_incentivo_cadastro_FINAL
 SELECT CAST(CONCAT('55',CELULAR) AS NVARCHAR(20)) AS phone
       ,CAST(PRIMEIRO_NOME AS NVARCHAR(20)) AS chicoFirstName
-      ,CAST([HASH] AS NVARCHAR(5)) AS chicoLinkHash
-      ,CAST('https://unilevertrade.s3.dualstack.sa-east-1.amazonaws.com/WhatsApp_Botmaker/000_Trade_UFS_Promos_de_junho_00-00_wpp.png' AS VARCHAR(1000)) AS imageURL
-  FROM Unilever_000_DBM.dbo.[20250620-adhoc-wpp-arraia_de_ofertas]
+  FROM Unilever_000_DBM.dbo.[20250625_adhoc_wpp_relacionamento_incentivo_cadastro]
--- data/sql/CBE.sql
+++ data/sql/CBE.sql
@@ -1,3 +1,3 @@
---20250618-adhoc-newbacon-wpp_feriado_corpus
-SELECT AdAgency_ADDB.dbo.fc_hash_sha2_256(EMAIL) AS [external_id]
-  FROM Ambev_001_DBM.dbo.TMP_BASE_WHATS_FERIADO_CORPUS_20250618
+--20250625-adhoc-newbacon-wpp_choppback
+SELECT AdAgency_ADDB.dbo.fc_hash_sha2_256(EMAIL) AS external_id
+  FROM Ambev_001_DBM.dbo.TMP_BASE_WHATS_CHOPPBACK_20250625
